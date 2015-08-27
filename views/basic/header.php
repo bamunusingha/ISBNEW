d4861b8c536972d9ccd2456169ed808419ebb72e
vishrajakaruna <vishrajakaruna@gmail.com>
2015-08-27 21:48:41 +0530

    second commit

--- views/basic/header.php
+++ views/basic/header.php
@@ -4,4 +4,16 @@
  * User: vishwa
  * Date: 8/27/2015
  * Time: 8:47 PM
- */ 
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!--Insert Bootstrap css, Jquery and Bootstrap JS-->
+    <meta charset="UTF-8">
+    <title>ISB APP</title>
+</head>
+<body>
+
+</body>
+</html>
