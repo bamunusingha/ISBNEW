8f1774e4ca31d4933b5048cb353b97cb18be98ee
vishrajakaruna <vishrajakaruna@gmail.com>
2015-08-27 21:41:10 +0530

    init commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+        This is the main template file. please use this only
+        to login to the application.
+    */
+
+    /*
+        include the header here
+    */
+
+?>
---
+++ views/basic/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vishwa
+ * Date: 8/27/2015
+ * Time: 8:47 PM
+ */ 
